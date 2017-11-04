80b0ea6a37467fc392c3bbc9fb4322c524cdca17
joextodd <joextodd@gmail.com>
2017-11-04 23:47:54 +0000

    Getting actual data now

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -6,12 +6,15 @@ extern "C" {
 void rom_i2c_writeReg_Mask(int, int, int, int, int, int);
 }
 
-#define I2SI_DATA       12
-#define I2SI_BCK        13
-#define I2SI_WS         14
+#include <ESP8266WiFi.h>
 
-#define SLC_BUF_CNT (4)    // Number of buffers in the I2S circular buffer
-#define SLC_BUF_LEN (512)  // Length of one buffer, in 32-bit words.
+#define I2SI_PWR        4u
+#define I2SI_DATA       12    // I2S data on GPIO12
+#define I2SI_BCK        13    // I2S clk on GPIO13
+#define I2SI_WS         14    // I2S select on GPIO14
+
+#define SLC_BUF_CNT     4     // Number of buffers in the I2S circular buffer
+#define SLC_BUF_LEN     512   // Length of one buffer, in 32-bit words.
 
 typedef struct {
   uint32_t blocksize:12;
@@ -25,13 +28,9 @@ typedef struct {
   uint32_t next_link_ptr;
 } sdio_queue_t;
 
-static volatile uint32_t i2s_slc_queue[SLC_BUF_CNT - 1];
-static volatile uint8_t i2s_slc_queue_len;
+static sdio_queue_t i2s_slc_items[SLC_BUF_CNT];  // I2S DMA buffer descriptors
 static uint32_t *i2s_slc_buf_pntr[SLC_BUF_CNT];  // Pointer to the I2S DMA buffer data
-static sdio_queue_t i2s_slc_items[SLC_BUF_CNT]; // I2S DMA buffer descriptors
 static volatile uint32_t rx_buf_cnt = 0;
-
-sdio_queue_t i2s_rx_queue1, i2s_rx_queue2, i2s_rx_queue3;
 
 void i2s_init();
 void slc_init();
@@ -41,9 +40,16 @@ void slc_isr(void *para);
 void
 setup()
 {
+  rx_buf_cnt = 0;
+
+  pinMode(I2SI_PWR, OUTPUT);
   pinMode(I2SI_WS, OUTPUT);
   pinMode(I2SI_BCK, OUTPUT);
   pinMode(I2SI_DATA, INPUT);
+  digitalWrite(I2SI_PWR, HIGH);
+
+  WiFi.forceSleepBegin();
+  delay(500);
 
   Serial.begin(115200);
   Serial.println("I2S receiver");
@@ -55,21 +61,40 @@ setup()
   Serial.println("Initialised");
 }
 
+/*
+ * TODO: Fix need to wiggle LRCL to start data.
+ */
 void
 loop()
 {
-   delay(1000);
-   Serial.println(rx_buf_cnt);
-   rx_buf_cnt = 0;
+  uint32_t cnt;
+
+  if (cnt != rx_buf_cnt) {
+    for (int y = 0; y < SLC_BUF_LEN; y++) {
+      if (i2s_slc_buf_pntr[rx_buf_cnt][y] > 0) {
+        uint32_t value = ~(i2s_slc_buf_pntr[rx_buf_cnt][y]) & 0xFFFFFF;
+        uint32_t mask = value & 0x800000;
+        int32_t output = ((value & 0xFF0000) >> 16) | (value & 0xFF00) | ((value & 0xFF) << 16);
+        if (mask) {
+          output = output * -1;
+        }
+        Serial.println(output);
+      }
+    }
+    cnt = rx_buf_cnt;
+  }
 }
 
+/*
+ * Initialise I2S as a RX master.
+ */
 void
 i2s_init()
 {
   // Config RX pin function
   PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_I2SI_DATA);
   PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_I2SI_BCK);
-  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U, FUNC_I2SI_WS);
+//  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U, FUNC_I2SI_WS);
 
   // Enable a 160MHz clock
   I2S_CLK_ENABLE();
@@ -79,34 +104,43 @@ i2s_init()
   I2SC |= I2SRST;
   I2SC &= ~(I2SRST);
 
-  // Set RX FIFO_MOD = 0 and disable DMA (FIFO only)
-  I2SFC &= ~(I2SDE | (I2SRXFMM << I2SRXFM));
+  // Reset DMA
+  I2SFC &= ~(I2SDE | (I2STXFMM << I2STXFM) | (I2SRXFMM << I2SRXFM));
+
   // Enable DMA
   I2SFC |= I2SDE;
 
-  // Set RX CHAN_MOD = 0
-  I2SCC &= ~(I2SRXCMM << I2SRXCM);
-  i2s_set_rate(44100);
+  // Set RX single channel (right)
+  I2SCC &= ~((I2STXCMM << I2STXCM) | (I2SRXCMM << I2SRXCM));
+  I2SCC |= (1 << I2SRXCM);
+  i2s_set_rate(96000);
+
+  // Set RX data to be received
+  I2SRXEN = SLC_BUF_LEN;
 
   // Start receiver
   I2SC |= I2SRXS;
 }
 
+/*
+ * Set I2S clock
+ */
 void
 i2s_set_rate(uint32_t rate)
 {
   uint32_t i2s_clock_div = (I2SBASEFREQ / (rate * 32)) & I2SCDM;
-  uint8_t i2s_bck_div = (I2SBASEFREQ / (rate * i2s_clock_div * 2)) & I2SBDM;
-  Serial.printf("Rate %u Div %u Bck %u Freq %u\n", 
-  rate, i2s_clock_div, i2s_bck_div, I2SBASEFREQ / (i2s_clock_div * i2s_bck_div * 2));
+  uint8_t i2s_bck_div = (I2SBASEFREQ / (rate * i2s_clock_div)) & I2SBDM;
+  Serial.printf("Rate %u Div %u Bck %u Freq %u\n",
+  rate, i2s_clock_div, i2s_bck_div, I2SBASEFREQ / (i2s_clock_div * i2s_bck_div));
 
-  //!trans master, !bits mod, rece slave mod, rece msb shift, right first, msb right
-  I2SC &= ~(I2STSM | (I2SBMM << I2SBM) | (I2SBDM << I2SBD) | (I2SCDM << I2SCD));
-  I2SC |= I2SRF | I2SMR | I2SRMS | (24 << I2SBD) | (32 << I2SCD);
+  // RX master mode, RX MSB shift, right first, msb right
+  I2SC &= ~(I2STSM | I2SRSM | (I2SBMM << I2SBM) | (I2SBDM << I2SBD) | (I2SCDM << I2SCD));
+  I2SC |= I2SRF | I2SMR | I2SRMS | ((i2s_bck_div - 1) << I2SBD) | ((i2s_clock_div - 1) << I2SCD);
 }
 
-// Initialize the SLC module for DMA function
-// https://github.com/esp8266/Arduino/blob/master/cores/esp8266/core_esp8266_i2s.c
+/*
+ * Initialize the SLC module for DMA operation
+ */
 void
 slc_init()
 {
@@ -124,6 +158,7 @@ slc_init()
     i2s_slc_items[x].next_link_ptr = (int)((x < (SLC_BUF_CNT - 1)) ? (&i2s_slc_items[x + 1]) : (&i2s_slc_items[0]));
   }
 
+  // Reset DMA
   ETS_SLC_INTR_DISABLE();
   SLCC0 |= SLCRXLR | SLCTXLR;
   SLCC0 &= ~(SLCRXLR | SLCTXLR);
@@ -132,45 +167,49 @@ slc_init()
   // Configure DMA
   SLCC0 &= ~(SLCMM << SLCM);      // Clear DMA MODE
   SLCC0 |= (1 << SLCM);           // Set DMA MODE to 1
-  SLCRXDC |= SLCBINR | SLCBTNR;   // Enable INFOR_NO_REPLACE and TOKEN_NO_REPLACE
-  SLCRXDC &= ~(SLCBRXFE | SLCBRXEM | SLCBRXFM);  // disable RX_FILL, RX_EOF_MODE and RX_FILL_MODE
+//  SLCRXDC |= SLCBINR | SLCBTNR;   // Enable INFOR_NO_REPLACE and TOKEN_NO_REPLACE
+//  SLCRXDC &= ~(SLCBRXFE | SLCBRXEM | SLCBRXFM);
 
   // Feed DMA the 1st buffer desc addr
-  // To send data to the I2S subsystem, counter-intuitively we use the RXLINK part, not the TXLINK as you might
-  // expect. The TXLINK part still needs a valid DMA descriptor, even if it's unused: the DMA engine will throw
-  // an error at us otherwise. Just feed it any random descriptor.
-  SLCTXL &= ~(SLCTXLAM << SLCTXLA);  // clear TX descriptor address
-  SLCTXL |= (uint32_t)&i2s_slc_items[0] << SLCTXLA;  // set TX descriptor address. any random desc is OK, we don't use TX but it needs to be valid
+  // To receive data from the I2S slave,
+  // counter intuitively we need to use the TXLINK.
+  SLCTXL &= ~(SLCTXLAM << SLCTXLA);
+  SLCTXL |= (uint32_t)&i2s_slc_items[0] << SLCTXLA;
 
   ETS_SLC_INTR_ATTACH(slc_isr, NULL);
-  
+
   // Enable EOF interrupt
-  SLCIE = SLCITXEOF;  
+  SLCIE = SLCITXEOF;
   ETS_SLC_INTR_ENABLE();
 
   // Start transmission
   SLCTXL |= SLCTXLS;
 }
 
+/*
+ * Triggered when SLC has finished writing
+ * to one of the buffers.
+ */
 void
 slc_isr(void *para)
 {
-  uint32_t slc_intr_status;
-  
-  slc_intr_status = SLCIS;
+  uint32_t status;
+
+  status = SLCIS;
   SLCIC = 0xFFFFFFFF;
-  
-  if (slc_intr_status == 0) {
+
+  if (status == 0) {
     return;
   }
 
-  if (slc_intr_status & SLCITXEOF) {
+  if (status & SLCITXEOF) {
+    // We have received a frame
     ETS_SLC_INTR_DISABLE();
-    sdio_queue_t *finished_item = (sdio_queue_t*)SLCTXEDA;
-    i2s_slc_queue[i2s_slc_queue_len] = finished_item->buf_ptr;
+    sdio_queue_t *finished = (sdio_queue_t*)SLCTXEDA;
+
     // Thanks to cnlohr for the undocumented magic
-    finished_item->owner = 1;
-    rx_buf_cnt++;
+    finished->owner = 1;
+    rx_buf_cnt = (rx_buf_cnt + 1) % SLC_BUF_CNT;
     ETS_SLC_INTR_ENABLE();
   }
 }
